8d61ef503e5aae72bc12f9e42088396350523daa
tunc gulec <tuncgulec@gmail.com>
2021-10-26 19:58:16 +0300

    Sample
    Sample Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace ACXVoip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axVoipActiveX1 = new AxTncVoipActiveX.AxVoipActiveX();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TxtServer = new System.Windows.Forms.TextBox();
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtProxy = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtExtension = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axVoipActiveX1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axVoipActiveX1
+            // 
+            this.axVoipActiveX1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.axVoipActiveX1.Location = new System.Drawing.Point(0, 311);
+            this.axVoipActiveX1.Name = "axVoipActiveX1";
+            this.axVoipActiveX1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVoipActiveX1.OcxState")));
+            this.axVoipActiveX1.Size = new System.Drawing.Size(974, 355);
+            this.axVoipActiveX1.TabIndex = 0;
+            this.axVoipActiveX1.OnInCall += new AxTncVoipActiveX.IVoipActiveXEvents_OnInCallEventHandler(this.axVoipActiveX1_OnInCall);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect Voip";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TxtExtension);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.TxtProxy);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.TxtPassword);
+            this.groupBox1.Controls.Add(this.TxtUsername);
+            this.groupBox1.Controls.Add(this.TxtServer);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(441, 270);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection Settings";
+            // 
+            // TxtServer
+            // 
+            this.TxtServer.Location = new System.Drawing.Point(190, 25);
+            this.TxtServer.Name = "TxtServer";
+            this.TxtServer.Size = new System.Drawing.Size(230, 26);
+            this.TxtServer.TabIndex = 2;
+            // 
+            // TxtUsername
+            // 
+            this.TxtUsername.Location = new System.Drawing.Point(190, 57);
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(230, 26);
+            this.TxtUsername.TabIndex = 3;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(190, 89);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(230, 26);
+            this.TxtPassword.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Server:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Proxy:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TxtProxy
+            // 
+            this.TxtProxy.Location = new System.Drawing.Point(190, 121);
+            this.TxtProxy.Name = "TxtProxy";
+            this.TxtProxy.Size = new System.Drawing.Size(230, 26);
+            this.TxtProxy.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Extension:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TxtExtension
+            // 
+            this.TxtExtension.Location = new System.Drawing.Point(190, 158);
+            this.TxtExtension.Name = "TxtExtension";
+            this.TxtExtension.Size = new System.Drawing.Size(230, 26);
+            this.TxtExtension.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 666);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.axVoipActiveX1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axVoipActiveX1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxTncVoipActiveX.AxVoipActiveX axVoipActiveX1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TxtExtension;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtProxy;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.TextBox TxtServer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACXVoip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axVoipActiveX1.Server = TxtServer.Text;
+            axVoipActiveX1.Password = TxtPassword.Text;
+            axVoipActiveX1.Proxy = TxtProxy.Text;
+            axVoipActiveX1.Extension = TxtExtension.Text;
+            axVoipActiveX1.Username = TxtUsername.Text;
+            axVoipActiveX1.ConnectVoip();
+
+            if (axVoipActiveX1.Connected == 1)
+                MessageBox.Show("Connected");
+            else
+                MessageBox.Show("No Connected"); 
+
+        }
+
+        private void axVoipActiveX1_OnInCall(object sender, AxTncVoipActiveX.IVoipActiveXEvents_OnInCallEvent e)
+        {
+            MessageBox.Show(e.number);
+        }
+    }
+}
